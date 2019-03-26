12ed83acccf1f87dcbf8047af85090bad23f0815
Henri d'Auvigny <henri.dauvigny@gmail.com>
2019-03-26 12:10:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 mkdir -p ${MOUNT_DIR}
-gcsfuse --key-file ${KEY_FILE} ${GCS_BUCKET} ${MOUNT_DIR}
+
+ADDITIONNAL_PARAMETER=""
+
+if [ -n "${KEY_FILE}" ]; then;
+  ADDITIONNAL_PARAMETER="--key-file ${KEY_FILE}"
+fi
+
+gcsfuse ${ADDITIONNAL_PARAMETER} ${GCS_BUCKET} ${MOUNT_DIR}
 exec $@
