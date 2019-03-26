c0a86bb0aed88b4eed7d72d909e08c09d6dd4b3d
Henres <henri.dauvigny@link-value.fr>
2019-03-26 12:28:21 +0100

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@ mkdir -p ${MOUNT_DIR}
 
 ADDITIONNAL_PARAMETER=""
 
-if [ -n "${KEY_FILE}" ]; then;
+if [ -n "${KEY_FILE}" ]
+then
   ADDITIONNAL_PARAMETER="--key-file ${KEY_FILE}"
 fi
 
-gcsfuse ${ADDITIONNAL_PARAMETER} ${GCS_BUCKET} ${MOUNT_DIR}
+echo "${ADDITIONNAL_PARAMETER}" "${GCS_BUCKET}" "${MOUNT_DIR}"
 exec $@
